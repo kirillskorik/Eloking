09d48012d6c5322e72d0a83defa7a426a0001195
kirillskorik <kirill.skorik.dev@gmail.com>
2023-07-27 14:57:43 +0300

    add error bundle

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById("submit").onclick = function showError() {
+    document.getElementById("error").style.display = "flex";
+    document.getElementById("error").style.visibility = "visible";
+    }
